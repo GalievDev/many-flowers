5bbcde4e3b062a652fcb10a550eec25d2791a9bf
Galiev <ggvalion@gmail.com>
2023-03-12 16:04:04 +0500

    Data Gen 1.19.3

---
+++ src/main/java/net/valion/manyflowers/data/ModWorldGenerator.java
@@ -0,0 +1,26 @@
+package net.valion.manyflowers.data;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.valion.manyflowers.ManyFlowers;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenerator extends FabricDynamicRegistryProvider {
+    public ModWorldGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup registries, Entries entries) {
+        entries.addAll(registries.getWrapperOrThrow(RegistryKeys.CONFIGURED_FEATURE));
+        entries.addAll(registries.getWrapperOrThrow(RegistryKeys.PLACED_FEATURE));
+    }
+
+    @Override
+    public String getName() {
+        return ManyFlowers.MOD_ID;
+    }
+}
