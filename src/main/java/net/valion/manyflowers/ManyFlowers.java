045a8e6c0f06c51430c38c8d1ee81c4730c33754
galiev <ggvalion@gmail.com>
2023-08-12 14:13:35 +0300

    Update

--- src/main/java/net/valion/manyflowers/ManyFlowers.java
+++ src/main/java/net/valion/manyflowers/ManyFlowers.java
@@ -5,9 +5,7 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.valion.manyflowers.config.MFConfig;
 import net.valion.manyflowers.helpers.WorldsHelper;
 import net.valion.manyflowers.particle.FlowerParticles;
@@ -41,30 +39,7 @@ public class ManyFlowers implements ModInitializer {
 
         ModWorldGen.generateModWorldGen();
 
-        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-            server.getWorlds().forEach(WorldsHelper::putWorld);
-            var world = server.getWorld(World.OVERWORLD);
-
-            for (Map.Entry<BlockPos, Integer> entry : blockChanges.entrySet()) {
-                var pos = entry.getKey();
-                var ticksRemaining = entry.getValue();
-                assert world != null;
-                var block = world.getBlockState(pos).getBlock();
-
-                if (ticksRemaining <= 0) {
-                    if (block == Blocks.GRASS_BLOCK) {
-                        world.setBlockState(pos, Blocks.CRIMSON_NYLIUM.getDefaultState());
-                    } else if (block == Blocks.DIRT) {
-                        world.setBlockState(pos, Blocks.NETHERRACK.getDefaultState());
-                    } else if (block == Blocks.STONE) {
-                        world.setBlockState(pos, Blocks.BASALT.getDefaultState());
-                    }
-                    blockChanges.remove(pos);
-                } else {
-                    entry.setValue(ticksRemaining - 1);
-                }
-            }
-        });
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> server.getWorlds().forEach(WorldsHelper::putWorld));
         DuckyUpdater.checkForUpdate("QUH6A4xu", MOD_ID);
     }
 }
--- src/main/java/net/valion/manyflowers/block/flowers/Velvets.java
+++ src/main/java/net/valion/manyflowers/block/flowers/Velvets.java
@@ -1,18 +1,16 @@
 package net.valion.manyflowers.block.flowers;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.FlowerBlock;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Random;
-
-import static net.valion.manyflowers.ManyFlowers.blockChanges;
-import static net.valion.manyflowers.setup.Flowers.VELVETS;
 
 public class Velvets extends FlowerBlock {
     public Velvets(StatusEffect suspiciousStewEffect, int effectDuration, Settings settings) {
@@ -21,18 +19,35 @@ public class Velvets extends FlowerBlock {
 
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
-        super.onPlaced(world, pos, state, placer, itemStack);
-        Random random = new Random();
-        int duration = random.nextInt(2) + 3; // Случайное число от 3600 до 6000 (3-6 минут)
-        for (int dx = -1; dx <= 1; dx++) {
-            for (int dy = -1; dy <= 1; dy++) {
-                for (int dz = -1; dz <= 1; dz++) {
-                    BlockPos nearbyPos = pos.add(dx, dy, dz);
-                    if (world.getBlockState(nearbyPos).getBlock() != VELVETS) {
-                        blockChanges.put(nearbyPos, duration);
+        if (world.getRegistryKey() == World.OVERWORLD) {
+            for (BlockPos gPos : BlockPos.iterate(pos.add(-2, -1, -2), pos.add(2, 1, 2))) {
+                for (BlockPos xPos : BlockPos.iterate(pos.add(-4, -1, -1), pos.add(4, 1, 2))) {
+                    for (BlockPos zPos : BlockPos.iterate(pos.add(-1, -1, -4), pos.add(1, 1, 4))) {
+                        var gBlocks = world.getBlockState(gPos).getBlock();
+                        var xBlocks = world.getBlockState(xPos).getBlock();
+                        var zBLocks = world.getBlockState(zPos).getBlock();
+
+                        if (gBlocks == Blocks.GRASS_BLOCK) {
+                            world.setBlockState(gPos, Blocks.CRIMSON_NYLIUM.getDefaultState());
+                            world.setBlockState(xPos, Blocks.CRIMSON_PLANKS.getDefaultState());
+                            world.setBlockState(zPos, Blocks.ACACIA_PLANKS.getDefaultState());
+                        } else if (gBlocks == Blocks.DIRT) {
+                            world.setBlockState(gPos, Blocks.NETHERRACK.getDefaultState());
+                            world.setBlockState(xPos, Blocks.CHERRY_PLANKS.getDefaultState());
+                            world.setBlockState(zPos, Blocks.JUNGLE_PLANKS.getDefaultState());
+                        } else if (gBlocks == Blocks.STONE) {
+                            world.setBlockState(gPos, Blocks.BASALT.getDefaultState());
+                            world.setBlockState(xPos, Blocks.COBBLESTONE.getDefaultState());
+                            world.setBlockState(zPos, Blocks.BOOKSHELF.getDefaultState());
+                        }
                     }
                 }
             }
         }
+    }
+
+    @Override
+    protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+        return super.canPlantOnTop(floor, world, pos) || floor.isIn(BlockTags.NETHER_CARVER_REPLACEABLES) || floor.isIn(BlockTags.SAND);
     }
 }
