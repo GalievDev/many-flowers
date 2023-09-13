cccc02486147f88a2ce1b7bc8d69441753b719a5
galiev <ggvalion@gmail.com>
2023-09-13 22:27:04 +0300

    Improve code

--- src/main/java/net/valion/manyflowers/block/flowers/entity/VelvetsBlockEntity.java
+++ src/main/java/net/valion/manyflowers/block/flowers/entity/VelvetsBlockEntity.java
@@ -16,7 +16,7 @@ import java.util.Random;
 
 public class VelvetsBlockEntity extends BlockEntity {
     public static ArrayList<BlockPos> poses = new ArrayList<>();
-    private final int delay = 100;
+    private final int delay = 20;
     private int counter = 0;
 
     public VelvetsBlockEntity(BlockPos pos, BlockState state) {
@@ -24,15 +24,24 @@ public class VelvetsBlockEntity extends BlockEntity {
     }
 
     public static void tick(World world, BlockPos blockPos, BlockState state, VelvetsBlockEntity entity) {
+        if (world.isClient) return;
         if (entity.counter < 0) entity.counter = 0;
         if (entity.counter == entity.delay) {
             if (poses.isEmpty()) {
-                 if (world.getRegistryKey() == World.OVERWORLD) {
-                    for (int x = -2; x <= 2; x++) {
-                        for (int y = -1; y <= 1; y++) {
-                            for (int z = -2; z <= 2; z++) {
-                                var targetPos = blockPos.add(x, y, z);
-                                poses.add(targetPos);
+                if (world.getRegistryKey() == World.OVERWORLD) {
+                    for (int x = blockPos.getX() - 1 ; x <= blockPos.getX() + 1; x++) {
+                        for (int y = blockPos.getY() - 2; y <= blockPos.getY() - 1; y++) {
+                            for (int z = blockPos.getZ() - 1; z <= blockPos.getZ() + 1; z++) {
+                                var pos = new BlockPos(x, y, z);
+                                poses.add(pos);
+                            }
+                        }
+                    }
+                    for (int x = blockPos.getX() - 2 ; x <= blockPos.getX() + 2; x++) {
+                        for (int y = blockPos.getY() - 3; y <= blockPos.getY() - 2; y++) {
+                            for (int z = blockPos.getZ() - 2; z <= blockPos.getZ() + 2; z++) {
+                                var pos = new BlockPos(x, y, z);
+                                poses.add(pos);
                             }
                         }
                     }
@@ -41,35 +50,34 @@ public class VelvetsBlockEntity extends BlockEntity {
                 setBlock(world, poses);
                 entity.counter = 0;
             }
-        }
-        entity.counter++;
+        } else entity.counter++;
     }
 
     private static void setBlock(@NotNull World world, @NotNull List<BlockPos> poses) {
-        Random random = new Random();
-        var randPos = poses.get(random.nextInt(poses.size()));
-        ManyFlowers.LOGGER.info("Rand pos: " + randPos);
-        var block = world.getBlockState(randPos).getBlock();
-        var state = world.getBlockState(randPos);
+        Random random = new Random(System.currentTimeMillis());
+        var pos = poses.get(random.nextInt(poses.size()));
+        ManyFlowers.LOGGER.info("Rand pos: " + pos);
+        var block = world.getBlockState(pos).getBlock();
+        var state = world.getBlockState(pos);
         ManyFlowers.LOGGER.info("Block: " + block);
 
         if (state.isOf(Blocks.GRASS_BLOCK)) {
-            world.setBlockState(randPos, Blocks.CRIMSON_NYLIUM.getDefaultState());
+            world.setBlockState(pos, Blocks.CRIMSON_NYLIUM.getDefaultState());
         } else if (state.isIn(BlockTags.DIRT)) {
-            world.setBlockState(randPos, Blocks.NETHERRACK.getDefaultState());
+            world.setBlockState(pos, Blocks.NETHERRACK.getDefaultState());
         } else if (state.isIn(BlockTags.BASE_STONE_OVERWORLD)) {
-            world.setBlockState(randPos, Blocks.BASALT.getDefaultState());
+            world.setBlockState(pos, Blocks.BASALT.getDefaultState());
         } else if (state.isIn(BlockTags.SAND)) {
-            world.setBlockState(randPos, Blocks.SOUL_SAND.getDefaultState());
+            world.setBlockState(pos, Blocks.SOUL_SAND.getDefaultState());
         } else if (state.isOf(Blocks.WATER)) {
-            world.setBlockState(randPos, Blocks.LAVA.getDefaultState());
+            world.setBlockState(pos, Blocks.LAVA.getDefaultState());
         } else if (state.isIn(BlockTags.TERRACOTTA)) {
-            world.setBlockState(randPos, Blocks.SMOOTH_BASALT.getDefaultState());
-        } /*else {
-            world.addParticle(ParticleTypes.SMOKE, randPos.getX() + 0.1D, randPos.getY() + 0.1D, randPos.getZ() + 0.1D,
-                    0.2D, 0.3D, 0.2D);
-            world.playSound(randPos.getX(), randPos.getY(), randPos.getZ(), SoundEvents.BLOCK_FIRE_AMBIENT,
-                    SoundCategory.BLOCKS, 1F, 0.0F, true);
-        }*/
+            world.setBlockState(pos, Blocks.SMOOTH_BASALT.getDefaultState());
+        } else if (state.isIn(BlockTags.LOGS)) {
+            world.setBlockState(pos, Blocks.CRIMSON_STEM.getDefaultState());
+        } else if (state.isIn(BlockTags.LEAVES)) {
+            world.setBlockState(pos, Blocks.NETHER_WART.getDefaultState());
+        }
+        poses.clear();
     }
 }
