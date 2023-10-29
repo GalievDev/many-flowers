eee76355cedf41b2de3d1df7913850085b3cb949
galiev <ggvalion@gmail.com>
2023-10-29 21:21:53 +0300

    Fixing

--- src/main/java/net/valion/manyflowers/block/flowers/JackFlower.java
+++ src/main/java/net/valion/manyflowers/block/flowers/JackFlower.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
@@ -12,10 +13,11 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 
 import java.util.function.ToIntFunction;
 
-public class JackFlower extends FlowerBlock {
+public class JackFlower extends FlowerBlock implements Fertilizable {
     public static final BooleanProperty LIT = Properties.LIT;
     public JackFlower() {
         super(StatusEffects.FIRE_RESISTANCE, 0, FabricBlockSettings.create().mapColor(MapColor.DARK_GREEN).nonOpaque()
@@ -45,9 +47,24 @@ public class JackFlower extends FlowerBlock {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(LIT);
+        super.appendProperties(builder);
     }
 
     private static ToIntFunction<BlockState> createLightLevelFromLitBlockState(int litLevel) {
-        return state -> state.get(Properties.LIT) != false ? litLevel : 0;
+        return state -> state.get(Properties.LIT) ? litLevel : 0;
+    }
+
+    @Override
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state, boolean isClient) {
+        return false;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return false;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
     }
 }
