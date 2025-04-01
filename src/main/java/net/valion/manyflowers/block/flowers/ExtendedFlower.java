efa73b831b908e80ea150cc521ffce5ab3445399
Vazir Vadirov <ggvalion@gmail.com>
2025-04-01 14:10:24 +0500

    Fixed method

--- src/main/java/net/valion/manyflowers/block/flowers/ExtendedFlower.java
+++ src/main/java/net/valion/manyflowers/block/flowers/ExtendedFlower.java
@@ -10,14 +10,14 @@ import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 import org.jetbrains.annotations.Nullable;
 
 public class ExtendedFlower extends BlockWithEntity implements BlockEntityProvider {
-
     private final static MapCodec<ExtendedFlower> CODEC = createCodec(ExtendedFlower::new);
 
     protected ExtendedFlower(Settings settings) {
@@ -30,8 +30,8 @@ public class ExtendedFlower extends BlockWithEntity implements BlockEntityProvid
     }
 
     @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        return !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
+        return !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
     }
 
     public boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
