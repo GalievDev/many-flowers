24eaf036f359842d4d83290b62bba46870f37868
Vazir Vadirov <ggvalion@gmail.com>
2025-05-17 10:48:16 +0500

    Added textures and potted flower blocks

--- src/main/java/net/valion/manyflowers/client/ManyFlowersClient.java
+++ src/main/java/net/valion/manyflowers/client/ManyFlowersClient.java
@@ -28,6 +28,12 @@ public class ManyFlowersClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getMARIGOLD(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getDAISIES(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getZINNIA(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getCOSMOS(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPURPLE_CORNFLOWER(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPETUNIA(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getGERANIUM(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getBEGONIA(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getSNAPDRAGON(), RenderLayer.getCutout());
 
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getSWEET_ALYSSUM(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getGAILLARDIA(), RenderLayer.getCutout());
@@ -36,12 +42,27 @@ public class ManyFlowersClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getOENOTHERA(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getCHRYSANTHEMUM(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getAUTUMN_CROCUS(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getBLACK_EYED_SUSAN(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getCOREOPSIS(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getDAHLIA(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getLAVENDER(), RenderLayer.getCutout());
+
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getVELVETS(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getAUTUMN_ASTERS(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getBONE_FLOWER(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getTRADE_FLOWER(), RenderLayer.getCutout());
 
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_ALSTROEMERIA(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_HYDRANGEA(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_MARIGOLD(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_PURPLE_CORNFLOWER(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_PETUNIA(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_BEGONIA(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_SNAPDRAGON(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_BLACK_EYED_SUSAN(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_COREOPSIS(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_DAHLIA(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_LAVENDER(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_DAISIES(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_SWEET_ALYSSUM(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getPOTTED_GAILLARDIA(), RenderLayer.getCutout());
@@ -64,6 +85,9 @@ public class ManyFlowersClient implements ClientModInitializer {
 
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getJACK_FLOWER(), RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getROOT_OF_THE_WORLDS(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getETHEREAL_ORCHID(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getDREADPETAL(), RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlocksRegistry.INSTANCE.getBLINDBLOSSOM(), RenderLayer.getCutout());
 
         EntityRendererRegistry.register(EntitiesTypeRegistry.INSTANCE.getDREADPETAL_ENTITY(), EmptyRenderer::new);
         EntityRendererRegistry.register(EntitiesTypeRegistry.INSTANCE.getBLINDBLOSSOM_ENTITY(), EmptyRenderer::new);
